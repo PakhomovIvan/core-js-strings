35df196f0202ec64895b0ae04eaabb3f134c435a
Ivan Pakhomov <ip1133@ya.ru>
2023-12-20 21:40:52 +0300

    feat: feat tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -470,7 +470,7 @@ function extractEmails(str) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13( /* str */) {
+function encodeToRot13(/* str */) {
   // const iN = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
   // const oUT = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
   // let encrypt = '';
@@ -506,62 +506,8 @@ function encodeToRot13( /* str */) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(value) {
-const deckCard = [
-  'A♣',
-  '2♣',
-  '3♣',
-  '4♣',
-  '5♣',
-  '6♣',
-  '7♣',
-  '8♣',
-  '9♣',
-  '10♣',
-  'J♣',
-  'Q♣',
-  'K♣',
-  'A♦',
-  '2♦',
-  '3♦',
-  '4♦',
-  '5♦',
-  '6♦',
-  '7♦',
-  '8♦',
-  '9♦',
-  '10♦',
-  'J♦',
-  'Q♦',
-  'K♦',
-  'A♥',
-  '2♥',
-  '3♥',
-  '4♥',
-  '5♥',
-  '6♥',
-  '7♥',
-  '8♥',
-  '9♥',
-  '10♥',
-  'J♥',
-  'Q♥',
-  'K♥',
-  'A♠',
-  '2♠',
-  '3♠',
-  '4♠',
-  '5♠',
-  '6♠',
-  '7♠',
-  '8♠',
-  '9♠',
-  '10♠',
-  'J♠',
-  'Q♠',
-  'K♠',
-  ];
-  return deckCard.indexOf(value);
+function getCardId(/* value */) {
+  throw new Error('Not implemented');
 }
 
 module.exports = {
