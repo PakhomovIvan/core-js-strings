0199b5d8a88cd1bef328bfd27fc837f37e86e8e8
Ivan Pakhomov <ip1133@ya.ru>
2023-12-20 21:38:45 +0300

    feat: feat new tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -470,15 +470,16 @@ function extractEmails(str) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(str) {
-  const iN = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-  const oUT = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
-  let encrypt = '';
-  for (let i = 0; i < str.length; i + 1) {
-    const item = iN.indexOf(str[i]);
-    encrypt += oUT[item];
-  }
-  return encrypt;
+function encodeToRot13( /* str */) {
+  // const iN = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  // const oUT = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+  // let encrypt = '';
+  // for (let i = 0; i < str.length; i + 1) {
+  //   const item = iN.indexOf(str[i]);
+  //   encrypt += oUT[item];
+  // }
+  // return encrypt;
+  throw new Error('Not implemented');
 }
 
 /**
@@ -505,8 +506,62 @@ function encodeToRot13(str) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+const deckCard = [
+  'A♣',
+  '2♣',
+  '3♣',
+  '4♣',
+  '5♣',
+  '6♣',
+  '7♣',
+  '8♣',
+  '9♣',
+  '10♣',
+  'J♣',
+  'Q♣',
+  'K♣',
+  'A♦',
+  '2♦',
+  '3♦',
+  '4♦',
+  '5♦',
+  '6♦',
+  '7♦',
+  '8♦',
+  '9♦',
+  '10♦',
+  'J♦',
+  'Q♦',
+  'K♦',
+  'A♥',
+  '2♥',
+  '3♥',
+  '4♥',
+  '5♥',
+  '6♥',
+  '7♥',
+  '8♥',
+  '9♥',
+  '10♥',
+  'J♥',
+  'Q♥',
+  'K♥',
+  'A♠',
+  '2♠',
+  '3♠',
+  '4♠',
+  '5♠',
+  '6♠',
+  '7♠',
+  '8♠',
+  '9♠',
+  '10♠',
+  'J♠',
+  'Q♠',
+  'K♠',
+  ];
+  return deckCard.indexOf(value);
 }
 
 module.exports = {
